5cc5a9c5fa118968cb21d595c68f78953c5ef24d
Jose C. Quiroga Garre <josecquiroga@gmail.com>
2016-05-12 21:19:00 +0200

    Subida final

---
+++ Features/Feature1/Feature1.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="c2e04e49-636b-4f6a-b80d-e053d11f6d21" description="SharePoint Add-in Feature" featureId="c2e04e49-636b-4f6a-b80d-e053d11f6d21" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="IntegracionECT Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="68eab3f9-de37-4527-ad46-f7f8fa8503d3" />
+    <projectItemReference itemId="6d285b0a-3772-4724-ad75-9f96d602ddac" />
+    <projectItemReference itemId="67a7878b-0e0f-41cf-b528-a29759edc0c8" />
+    <projectItemReference itemId="907a8c16-f44d-4a5d-a980-f9f72c3dfaf8" />
+    <projectItemReference itemId="b8aeb5e8-0561-4ccb-a7c0-c7570a67df05" />
+    <projectItemReference itemId="6107545b-5547-4ffc-bd64-a94b595ed36d" />
+  </projectItems>
+</feature>
---
+++ Scripts/App.js
@@ -0,0 +1,68 @@
+﻿'use strict';
+
+function getPersonas() {
+    var nom = $("#txtNombre").val();
+
+    $("#resultado").empty();
+
+    var uri = _spPageContextInfo.webAbsoluteUrl + "/_api/lists/getByTitle('Trabajador')/items?$filter=nombre eq '" + nom + "'";
+
+    $.ajax({
+        type: "GET",
+        url: uri,
+        contenType: "application/json",
+        headers: {
+            "accept": "application/json;odata=verbose"
+        },
+        success: onGetresults,
+        error: onError
+    });
+}
+function getPersonas2() {
+    var nom = $("#txtNombre").val();
+
+    $("#resultado").empty();
+
+    var uri = _spPageContextInfo.webAbsoluteUrl + "/_api/lists/getByTitle('Trabajador')/items?$filter=apellidos eq '" + nom + "'";
+
+    $.ajax({
+        type: "GET",
+        url: uri,
+        contenType: "application/json",
+        headers: {
+            "accept": "application/json;odata=verbose"
+        },
+        success: onGetresults,
+        error: onError
+    });
+}
+
+function onError() {
+    alert(r.status);
+}
+
+function onGetresults(xhr) {
+    var odata = xhr.d.results;
+    var res = "<table>" +
+        "<thead>" +
+        "<th>Nombre</th>" +
+        "<th>Apellidos</th>" +
+        "<th>Edad</th>" +
+        "<th>Salario</th>" +
+        "</thead>";
+    $.each(odata, function (i, item) {
+        res += "<tr>";
+        res += "<td>" + item.nombre + "</td>";
+        res += "<td>" + item.apellidos + "</td>";
+        res += "<td>" + item.edad + "</td>";
+        res += "<td>" + item.salario + "</td>";
+        res += "</tr>";
+    });
+    res += "</table>";
+    $("#resultado").html(res);
+}
+
+$(document).ready(function () {
+    $("#btnOK").click(getPersonas);
+    $("#btnOK2").click(getPersonas2);
+});
